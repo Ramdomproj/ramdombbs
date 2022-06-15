50cee975b3a5cace0592b98592a68d8c17bcca21
dongdigua <dongdigua@outlook.com>
2022-06-15 10:20:12 +0800

    包子快弄页面

---
+++ lib/randombbs/db.ex
@@ -0,0 +1,3 @@
+defmodule Randombbs.DB do
+
+end
--- lib/randombbs/router.ex
+++ lib/randombbs/router.ex
@@ -5,15 +5,30 @@ defmodule Randombbs.Router do
   plug Plug.Static,
     at: "/",
     from: "www",
-    only: ["user", "images", "js", "css", "home.html"]
+    only: ["user", "images", "assets", "home.html", "robots.txt"]
 
-  plug(Plug.Parsers, parsers: [:urlencoded])
+  plug Plug.Parsers,
+    parsers: [:urlencoded]
   plug :match
   plug :dispatch
 
   get "/" do
     home_page = File.read!("www/home.html")
     send_resp(conn, 200, home_page)
+  end
+
+  get "/hello/:name" do
+    send_resp(conn, 200, "this url is for testing\nhello #{name}")
+  end
+
+  post "/signup" do
+    IO.inspect(conn)
+    send_resp(conn, 200, "sign up successful...才怪")
+  end
+
+  post "/login" do
+    IO.inspect(conn)
+    send_resp(conn, 200, "log in successful...才怪")
   end
 
   match _ do
